6ab486653bdd61b17cd177750a2b0bb5f5b1fea0
iWillyWonka <81636190+iWillyWonka@users.noreply.github.com>
2021-04-24 21:34:09 +0300

    HelloW

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
-const burgerIcon = document.querySelector(".burger-icon");
-const navbar = document.querySelector(".navbar");
+// Burger
+const iconMenu = document.querySelector('.menu_icon');
+const menuBody = document.querySelector('.menu');
+const menuSocialIcons = document.querySelector('.social-networks-fa-burger');
+const tel = document.querySelector('.phonecell');
 
-burgerIcon.addEventListener("click", function () {
-  navbar.classList.toggle("navbar_active");
+iconMenu.addEventListener('click', function (e) {
+  document.body.classList.toggle('_lock');
+  iconMenu.classList.toggle('_active');
+  menuBody.classList.toggle('_active');
+  menuSocialIcons.classList.toggle('_active');
+  tel.classList.toggle('_active');
 });
 
-navbar.addEventListener("click", function () {
-  navbar.classList.remove("navbar_active");
-});
+document
+  .querySelectorAll('.menu a')
+  .forEach((menuItem) => menuItem.addEventListener('click', hideMenu));
+
+  //Burger close after click
+function hideMenu() {
+  document.body.classList.remove('_lock');
+  iconMenu.classList.remove('_active');
+  menuBody.classList.remove('_active');
+  menuSocialIcons.classList.remove('_active');
+  tel.classList.remove('_active');
+}
